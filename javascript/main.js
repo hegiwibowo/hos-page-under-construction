1852121e012aa48c82937eb3c3a3c39cf241b536
Tashinga <mtokotashinga@gmail.com>
2019-10-05 20:00:04 +0200

    fixed protos

--- javascript/main.js
+++ javascript/main.js
@@ -10,5 +10,5 @@ left.addEventListener('mousemove', (event) => {
     const move2 = (event.clientX *0.003);
 
     content.style.transform = `translateX(-${move2}%)`;
-    protos.forEach(proto => proto.style.transform = `translateX(${move}%)`)
+    protos.forEach(proto => proto.style.transform = `translateX(${move}%) skew(30deg, 0deg)`)
 })
